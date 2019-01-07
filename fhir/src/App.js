849d0d93ad731b9ac9f350b28eb82d67706ea42d
karadalex <karadalex@gmail.com>
2019-01-07 19:26:53 +0200

    installed BigchainDB driver , execute and display signed Transaction

--- fhir/src/App.js
+++ fhir/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as driver from 'bigchaindb-driver';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      observation: {}
+      fhirData: {},
+      bdbTx: {}
     }
   }
 
@@ -22,17 +24,61 @@ class App extends Component {
       }).then((r) => {
          console.log(r);
          this.setState({
-           observation: r
+           fhirData: r
          })
       });
+
+    // BigchainDB server instance (e.g. https://example.com/api/v1/)
+    const API_PATH = 'https://test.bigchaindb.com/api/v1/'
+
+    // Create a new keypair.
+    const alice = new driver.Ed25519Keypair()
+
+    // Construct a transaction payload
+    const tx = driver.Transaction.makeCreateTransaction(
+        // Define the asset to store, in this example it is the current temperature
+        // (in Celsius) for the city of Berlin.
+        { city: 'Berlin, DE', temperature: 22, datetime: new Date().toString() },
+
+        // Metadata contains information about the transaction itself
+        // (can be `null` if not needed)
+        { what: 'My first BigchainDB transaction' },
+
+        // A transaction needs an output
+        [ driver.Transaction.makeOutput(
+                driver.Transaction.makeEd25519Condition(alice.publicKey))
+        ],
+        alice.publicKey
+    )
+
+    // Sign the transaction with private keys
+    const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
+
+    // Send the transaction off to BigchainDB
+    let conn = new driver.Connection(API_PATH)
+
+    conn.postTransactionCommit(txSigned)
+        .then(res => {
+            console.log('Transaction', txSigned.id, 'accepted')
+            this.setState({
+              bdbTx: txSigned
+            })
+        })
   }
 
   render() {
     return (
       <div className="App">
-        FHIR Frontend <br></br>
+        <h1>FHIR Frontend </h1>
+        <br></br>
+        <h2>FHIR Data</h2>
         <pre>
-          {JSON.stringify(this.state.observation, null, 2)}
+          {JSON.stringify(this.state.fhirData, null, 2)}
+        </pre>
+        <br></br>
+        <h2>BigchainDB Transaction</h2>
+        <pre>
+          {JSON.stringify(this.state.bdbTx, null, 2)}
         </pre>
       </div>
     );
