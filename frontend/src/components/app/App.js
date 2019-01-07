fcf22070482168fc646be25eca9b95cb6452fa4a
karadalex <karadalex@gmail.com>
2019-01-07 19:55:18 +0200

    deleted what was left from FHIR component

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import BDB from '../bdb/BDB';
 import FHIR from '../fhir/FHIR';
+import BDB from '../bdb/BDB';
+import IPFS from '../ipfs/IPFS';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      fhirData: {}
-    }
-  }
-
-  componentDidMount() {
-    // FHIR is globally loaded in index.js and can be accessed from the window variable
-    var smart = window.FHIR.client({
-      serviceUrl: 'https://r2.smarthealthit.org',
-      auth: {
-        type: 'none'
-      }
-    });
-  
-    smart.api.search({type: "Observation", query: {subject: "99912345"}
-      }).then((r) => {
-         console.log(r);
-         this.setState({
-           fhirData: r
-         })
-      });
+    this.state = {}
   }
 
   render() {
@@ -37,6 +18,8 @@ class App extends Component {
         <FHIR></FHIR>
         <br></br>
         <BDB></BDB>
+        <br></br>
+        <IPFS></IPFS>
       </div>
     );
   }
