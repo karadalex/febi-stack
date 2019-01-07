2296bf251fad8f5cca4537fc7a08c423765aeb55
karadalex <karadalex@gmail.com>
2019-01-07 20:18:00 +0200

    working IPFS component

--- frontend/src/components/ipfs/IPFS.js
+++ frontend/src/components/ipfs/IPFS.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({
+    host: 'ipfs.infura.io',
+    port: '5001',
+    protocol: 'https'
+})
+const stringToUse = 'hello world from webpacked IPFS'
 
 class IPFS extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      fhirData: {}
+      id: null,
+      version: null,
+      protocol_version: null,
+      added_file_hash: null,
+      added_file_contents: null
     }
   }
-
-  componentDidMount() {
-    // FHIR is globally loaded in index.js and can be accessed from the window variable
-    var smart = window.FHIR.client({
-      serviceUrl: 'https://r2.smarthealthit.org',
-      auth: {
-        type: 'none'
-      }
-    });
-  
-    smart.api.search({type: "Observation", query: {subject: "99912345"}
-      }).then((r) => {
-         console.log(r);
-         this.setState({
-           fhirData: r
-         })
-      });
+  componentDidMount () {
+    ipfs.id((err, res) => {
+      if (err) throw err
+      this.setState({
+        id: res.id,
+        version: res.agentVersion,
+        protocol_version: res.protocolVersion
+      })
+    })
+    ipfs.add([Buffer.from(stringToUse)], (err, res) => {
+        if (err) throw err
+        const hash = res[0].hash
+        this.setState({added_file_hash: hash})
+        ipfs.cat(hash, (err, file) => {
+            if (err) throw err
+            let data = file.toString('utf8')
+            this.setState({added_file_contents: data})
+        })
+    })
   }
-
-  render() {
+  render () {
     return (
-      <div className="ipfs">
-        <h2>FHIR Data</h2>
-        <pre>
-          {JSON.stringify(this.state.fhirData, null, 2)}
-        </pre>
-      </div>
+        <div>
+            <h2>IPFS Files</h2>
+            <p>File ID: <strong>{this.state.id}</strong></p>
+            <p>IPFS version: <strong>{this.state.version}</strong></p>
+            <p>IPFS protocol version: <strong>{this.state.protocol_version}</strong></p>
+            <div>
+                <div>
+                File was added<br />
+                {this.state.added_file_hash}
+                </div>
+                <div>
+                Contents of file added: <br />
+                {this.state.added_file_contents}
+                </div>
+            </div>
+        </div>
     );
   }
 }
