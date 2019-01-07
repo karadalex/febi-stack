c0a8b341e4bbe45ded908953d89a14ef6b91916d
karadalex <karadalex@gmail.com>
2019-01-07 15:00:04 +0200

    installed fhir

--- fhir/src/App.js
+++ fhir/src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var smart = window.FHIR.client({
+      serviceUrl: 'https://fhir-open-api-dstu2.smarthealthit.org',
+      auth: {
+        type: 'none'
+      }
+    });
+  
+    smart.api.search({type: "Observation", query: {subject: "99912345"}
+      }).then(function(r){
+         console.log(JSON.stringify(r,null,2));
+      });
+
     return (
       <div className="App">
         FHIR Frontend
