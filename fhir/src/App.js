8f2585fa9cee5b6d2c76edde41ad062e6864a07d
karadalex <karadalex@gmail.com>
2019-01-07 15:37:55 +0200

    pretty print FHIR JSON Resource

--- fhir/src/App.js
+++ fhir/src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      observation: {}
+    }
+  }
+
+  componentDidMount() {
     // FHIR is globally loaded in index.js and can be accessed from the window variable
     var smart = window.FHIR.client({
       serviceUrl: 'https://r2.smarthealthit.org',
@@ -12,13 +19,21 @@ class App extends Component {
     });
   
     smart.api.search({type: "Observation", query: {subject: "99912345"}
-      }).then(function(r){
+      }).then((r) => {
          console.log(r);
+         this.setState({
+           observation: r
+         })
       });
+  }
 
+  render() {
     return (
       <div className="App">
-        FHIR Frontend
+        FHIR Frontend <br></br>
+        <pre>
+          {JSON.stringify(this.state.observation, null, 2)}
+        </pre>
       </div>
     );
   }
